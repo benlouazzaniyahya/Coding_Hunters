d658880b04a6cec4d1bdd01938360a5c1d328bf3
chaimaenassih <nassihchaimae30@gmail.com>
2025-05-29 03:46:55 +0200

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import WhyChooseUs from './components/WhyChooseUs';
@@ -6,17 +6,52 @@ import BlogsSection from './components/BlogsSection';
 import ContactSection from './components/ContactSection';
 import Testimonials from './components/Testimonials';
 import Footer from './components/Footer';
+import LoginModal from './components/LoginModal';
+import RegisterModal from './components/RegisterModal';
 
 function App() {
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+  const [isRegisterModalOpen, setIsRegisterModalOpen] = useState(false);
+
+  const openLoginModal = () => {
+    setIsLoginModalOpen(true);
+    setIsRegisterModalOpen(false);
+  };
+
+  const openRegisterModal = () => {
+    setIsRegisterModalOpen(true);
+    setIsLoginModalOpen(false);
+  };
+
+  const closeModals = () => {
+    setIsLoginModalOpen(false);
+    setIsRegisterModalOpen(false);
+  };
+
   return (
     <div className="min-h-screen">
-      <Header />
+      <Header
+        onOpenLogin={openLoginModal}
+        onOpenRegister={openRegisterModal}
+      />
       <HeroSection />
       <WhyChooseUs />
       <BlogsSection />
       <Testimonials />
       <ContactSection />
       <Footer />
+
+      {/* Modals */}
+      <LoginModal
+        isOpen={isLoginModalOpen}
+        onClose={closeModals}
+        onSwitchToRegister={openRegisterModal}
+      />
+      <RegisterModal
+        isOpen={isRegisterModalOpen}
+        onClose={closeModals}
+        onSwitchToLogin={openLoginModal}
+      />
     </div>
   );
 }
